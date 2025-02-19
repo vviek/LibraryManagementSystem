61a9749c7b9b63ca2ecaac0fba1a6fb46936e96a
Ashi Patel <ashipatel2102@gmail.com>
2025-02-19 20:22:09 +0530

    Upadate manage js

--- src/main/resources/static/js/register/manage.js
+++ src/main/resources/static/js/register/manage.js
@@ -3,7 +3,7 @@ function checkUsername()
 	
 	
 	
-	var userName=document.getElementById('name').value
+	var userName=document.getElementById('name').value;
 	
 	   if(userName.length > 0)
 		{
@@ -20,22 +20,49 @@ function checkEmailValidation()
 	
 	
 	
-	var userName=document.getElementById('email').value
+	var emailAddress=document.getElementById('email').value;
 	
-	if(userName.length > 0)
+	if(emailAddress.length > 0)
 		{
 			
 		}else{
 			
-			alert("Username cannot be blank.");
+			alert("Email cannot be blank.");
 		}
 	
+}
+
+function checkPassword()
+{
+	var password=document.getElementById('password').value;
+	
+	if(password.length > 0)
+		{
+		}
+	if(password.length>6){
+			alert("Password should be atleast 6 digit")
+		}
+		else{
+			alert("Password cannot be blank");
+		}
+}
+
+function checkConfirmPassword()
+{
+	var password=document.getElementById('confirmpassword').value;
+		
+		if(password.length > 0)
+			{
+			}
+			else{
+						alert("Password cannot be blank");
+					}
 }
 
 function checkAllValidation()
 {
 	
-	var userName=document.getElementById('name').value
+	var userName=document.getElementById('name').value;
 
 	   if(userName.length == 0)
 		{	
@@ -44,13 +71,24 @@ function checkAllValidation()
 			return;
 		}
 		
-		var emailData=document.getElementById('email').value
+		var emailData=document.getElementById('email').value;
 
 		if(emailData.length == 0)
 			{
 				alert("Email cannot be blank.");
 			}
 		
-	
-	
+		var passwordData=document.getElementById('password').value;
+		
+		if(passwordData.length==0)
+			{
+				alert("Password cannot be blank.")
+             }
+		var confirmPasswordData=document.getElementById('confirmpassword').value;
+						
+		if(confirmPasswordData.length==0)
+			{
+				alert("Password cannot be blank.");
+			}
 }
+
