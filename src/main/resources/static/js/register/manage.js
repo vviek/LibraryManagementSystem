2752556468087763e6ea705ff0829cee74b0765e
Vivek Patel <vivek.patel371@gmail.com>
2025-02-19 18:21:29 +0530

    JSCHanges

--- src/main/resources/static/js/register/manage.js
+++ src/main/resources/static/js/register/manage.js
@@ -1,5 +1,56 @@
-function checkConfirmPassword()
+function checkUsername()
 {
-	alert("sds");
+	
+	
+	
+	var userName=document.getElementById('name').value
+	
+	   if(userName.length > 0)
+		{
+			
+		}else{
+			
+			alert("Username cannot be blank.");
+		}
+	
+}
+
+function checkEmailValidation()
+{
+	
+	
+	
+	var userName=document.getElementById('email').value
+	
+	if(userName.length > 0)
+		{
+			
+		}else{
+			
+			alert("Username cannot be blank.");
+		}
+	
+}
+
+function checkAllValidation()
+{
+	
+	var userName=document.getElementById('name').value
+
+	   if(userName.length == 0)
+		{	
+			alert("Please enter Username.");
+			
+			return;
+		}
+		
+		var emailData=document.getElementById('email').value
+
+		if(emailData.length == 0)
+			{
+				alert("Email cannot be blank.");
+			}
+		
+	
 	
 }
