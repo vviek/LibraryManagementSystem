7f4bf612dcc3cc80d3731a332760d45c1dcdc07f
Ashi Patel <ashipatel2102@gmail.com>
2025-02-28 20:43:19 +0530

    RestControler

--- src/main/java/com/reactjs/backend/RestController/UserRestController.java
+++ src/main/java/com/reactjs/backend/RestController/UserRestController.java
@@ -1,5 +1,33 @@
 package com.reactjs.backend.RestController;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Backend.Service.AddressServiceData;
+import com.example.Backend.model.Address;
+
+@RestController
+@RequestMapping("/user")
+
 public class UserRestController {
+
+	@Autowired
+	 addressServiceDataObj;
+	
+	@GetMapping("/alladdress")
+	public ResponseEntity<List<Address>> getAllAddress(){
+		
+		List<Address> addressList= new ArrayList<>();
+		addressList= addressServiceDataObj.GetAllAddress();
+		return ResponseEntity.ok(addressList);
+		
+	}
 
 }
